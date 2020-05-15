ff8a4145a6c403a0e1323e67d743ab8c66d2b814
HHTupup <2633257902@qq.com>
2020-05-15 22:44:55 +0800

    game01

---
+++ src/play-ball/assets/prefabs/normal_ball_charger/script/normal_ball.js
@@ -0,0 +1,75 @@
+// Learn cc.Class:
+//  - https://docs.cocos.com/creator/manual/en/scripting/class.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+var com = require('common');
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        xSpeed: {
+            default: 0,
+            type: cc.Float
+        },
+
+        ySpeed: {
+            default: 0,
+            type: cc.Float
+        },
+
+        maxMoveSpeed: {
+            default: 18,
+            type: cc.Float
+        },
+
+        minMoveSpeed:{
+            default: 8,
+            type: cc.Float
+        },
+
+        collideAudio: {
+            default: null,
+            type: cc.AudioClip
+        }
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {
+        let manager = cc.director.getCollisionManager();
+        manager.enabled = true;
+        //manager.enabledDebugDraw = true;
+    },
+
+    start () {
+
+    },
+
+    ballMove () {
+        const canvas = this.node.parent.getComponent('charge_bar').Canvas;
+        if(Math.abs(this.node.x) + this.node.width/2 >= canvas.width/2) {
+            this.xSpeed = -this.xSpeed;
+            if(com.data == 1) {
+                cc.audioEngine.playEffect(this.collideAudio);
+            }
+        }
+        if(Math.abs(this.node.y) + this.node.height/2 >= canvas.height/2) {
+            this.ySpeed = -this.ySpeed;
+            if(com.data == 1) {
+                cc.audioEngine.playEffect(this.collideAudio);
+            }
+        }
+
+        // this.xSpeed = this.accel * this.xSpeed;
+        // this.ySpeed = this.accel * this.ySpeed;
+
+        this.node.x += this.xSpeed;
+        this.node.y += this.ySpeed;
+    },
+
+    update (dt) {
+        this.ballMove();
+    },
+});
---
+++ src/play-ball/assets/prefabs/trap/script/trap.js
@@ -0,0 +1,42 @@
+// Learn cc.Class:
+//  - https://docs.cocos.com/creator/manual/en/scripting/class.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        nextSceneName: {
+            default: 'game_01',
+        },
+
+        otherBallScript: {
+            default: 'normal_ball'
+        }
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {
+        let manager = cc.director.getCollisionManager();
+        manager.enabled = true;
+        // manager.enabledDebugDraw = true;
+    },
+
+    start () {
+
+    },
+
+    onCollisionEnter: function(other, self) {
+        const otherScript =  other.getComponent(this.otherBallScript);
+        otherScript.xSpeed = 0;
+        otherScript.ySpeed = 0;
+        console.log('fail');
+        cc.director.loadScene(this.nextSceneName);
+    },
+
+    // update (dt) {},
+});
