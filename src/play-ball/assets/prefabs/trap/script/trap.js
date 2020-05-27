8c21b2eb19a7d7f9151cda7f2ea71789a902a08f
dog-bear <550421489@qq.com>
2020-05-27 22:52:20 +0800

    修改过场动画

--- src/play-ball/assets/prefabs/trap/script/trap.js
+++ src/play-ball/assets/prefabs/trap/script/trap.js
@@ -1,15 +1,18 @@
-// Learn cc.Class:
-//  - https://docs.cocos.com/creator/manual/en/scripting/class.html
-// Learn Attribute:
-//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
 var com = require('common');
 cc.Class({
     extends: cc.Component,
 
     properties: {
-        circle_transition: {
+
+        //幕布开合时间
+        duration : 0.5,
+
+        top_Curtain: {
+            default: null,
+            type: cc.Node
+        },
+
+        bottom_Curtain: {
             default: null,
             type: cc.Node
         },
@@ -47,24 +50,25 @@ cc.Class({
         }
         //小球碰到陷阱后， 判定为失败，并且撤销陷阱的碰撞监听
         if(otherCollider.node._name == this.ball._name) {
-            this.open_the_door();
+            this.close_the_door();
             console.log('fail');
             com.result=-1;
             this.scheduleOnce(function() {
                 cc.director.loadScene(this.nextSceneName);
-            }, 0.3);
+            }, this.duration);
             this.node.getComponent(cc.RigidBody).enabledContactListener = false;
         }
     },
 
-    open_the_door:function(){
-        this.circle_transition.x=this.ball.x;
-        this.circle_transition.y=this.ball.y;
-        this.circle_transition.active=true;
-        cc.tween(this.circle_transition)
-            .to(0.3, { scale: 3 })
-            .start()
+    close_the_door:function(){
 
+        cc.tween(this.top_Curtain)
+        .to(this.duration, { position: cc.v2(0, 250) })
+        .start()
+        cc.tween(this.bottom_Curtain)
+        .to(this.duration, { position: cc.v2(0, -250) })
+        .start()
+        this.scheduleOnce(function() {
+        },this.duration);
     },
-    // update (dt) {},
 });
