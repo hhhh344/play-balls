65abd855597d157926ab7a8b00cbb2d815dbb387
Hubbard <810088953@qq.com>
2020-05-28 10:53:25 +0800

    完善关卡内容和删除冗余图片

--- src/play-ball/assets/scenes/game_13/script/groundTouch.js
+++ src/play-ball/assets/scenes/game_13/script/groundTouch.js
@@ -9,21 +9,9 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        circle_transition: {
-            default: null,
-            type: cc.Node
-        },
-
         ball: {
             default: null,
             type: cc.Node
-        },
-        nextSceneName: {
-            default: 'game_01',
-        },
-
-        otherBallScript: {
-            default: 'normal_ball'
         },
 
         collideAudio: {
@@ -78,15 +66,7 @@ cc.Class({
             console.log('后');
             other.getComponent(cc.RigidBody).linearVelocity = cc.Vec2.ZERO;
     },
-    open_the_door:function(){
-        this.circle_transition.x=this.ball.x;
-        this.circle_transition.y=this.ball.y;
-        this.circle_transition.active=true;
-        cc.tween(this.circle_transition)
-        .to(.5, { scale: 3 })
-        .start()
-             
-    },
+
     // update (dt) {
     // },
 });
